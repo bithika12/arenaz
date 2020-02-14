1d52c06c588aea6b3831bc5714427994a703cb69
Bithika-Redapple <bithika@redappletech.com>
2020-02-14 18:04:19 +0530

    profile controller updated v2

--- controllers/gameplay/GameRoomController.js
+++ controllers/gameplay/GameRoomController.js
@@ -962,7 +962,7 @@ io.on('connection', function (socket) {
     });
     socket.on('disconnecting', function (req) {
         let currentSocketId = socket.id;
-        logger.print("socket id while disconnecting"+ " " +currentSocketId)
+        logger.print("disconnecting 12222 socket id while disconnecting"+ " " +currentSocketId)
     })
 
     socket.on('disconnect', function (req) {
