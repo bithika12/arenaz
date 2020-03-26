775f4ab9e3e636b3bc820eb84dd2b69c3e844cf0
Bithika-Redapple <bithika@redappletech.com>
2020-03-26 16:29:36 +0530

    cupnumber chracter modified

--- controllers/gameplay/SocketJoinController.js
+++ controllers/gameplay/SocketJoinController.js
@@ -32,7 +32,8 @@ io.on('connection', function(socket){
 				packet[1].colorName = (!userDetails.colorName[0])?"":userDetails.colorName[0]['colorName'];
 				packet[1].raceName = (!userDetails.raceName[0])?"":userDetails.raceName[0]['raceName'];
 				packet[1].dartname = (!userDetails.dartName[0])?"":userDetails.dartName[0]['dartName'];
-				packet[1].cupNumbers = (!userDetails.cupNo)?"":userDetails.cupNo;
+				packet[1].cupNumbers = (!userDetails.cupNo)?0:userDetails.cupNo;
+				//packet[1].cupNumbers = (!userDetails.cupNo)?"":userDetails.cupNo;
 				//packet[1].colorName = (!userDetails.colorName[0]['colorName'])?"":userDetails.colorName[0]['colorName'];
 				//packet[1].raceName = (!userDetails.raceName[0]['raceName'])?"":userDetails.raceName[0]['raceName'];
               return next(); 
