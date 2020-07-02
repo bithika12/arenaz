62dd67366331c7d388ff00d55cf088429a087582
Bithika-Redapple <bithika@redappletech.com>
2020-07-02 15:58:16 +0530

    socket time modified

--- utils/SocketManager.js
+++ utils/SocketManager.js
@@ -3,7 +3,8 @@ var SocketManager = (function () {
     let instance;
 
     function init(server) {
-        this.io = require('socket.io')(server,{ wsEngine: 'ws' });
+        //this.io = require('socket.io')(server,{ wsEngine: 'ws' });
+        this.io= require('socket.io')(server, {'pingInterval': 4000, 'pingTimeout': 8000});
     }
 
     return {
