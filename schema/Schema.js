97388613d1fd2d5c308a6d4f579f2bcbc591042f
Bithika-Redapple <bithika@redappletech.com>
2020-09-04 14:30:01 +0530

    schema modified

--- schema/Schema.js
+++ schema/Schema.js
@@ -195,7 +195,7 @@ var userCoinSchema      = new Schema({
     user_name : String,   
     coins:Number,
     reference:String,
-    type :  {  type : String , enum: ['Deposit','Withdrawal','withdrawl','Withdrawl','Lost','Won'] , default : 'Deposit'}
+    type :  {  type : String , enum: ['Deposit','deposit','Withdrawal','withdrawl','Withdrawl','Lost','Won'] , default : 'Deposit'}
     
 });
 //MODEL DECLARATION
