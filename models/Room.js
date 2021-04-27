f468a8045f85134344ca6a45b3ed4c6a91749ddd
Bithika-Redapple <bithika@redappletech.com>
2021-04-27 20:25:01 +0530

    romm join modified

--- models/Room.js
+++ models/Room.js
@@ -236,7 +236,7 @@ room.createRoom = function(condObj){
                     });
                 }
                 else {
-                  if(responses[0].users[0]['userId'] !=condObj.userId){
+                 // if(responses[0].users[0]['userId'] !=condObj.userId){
                 updateObj = {}
                 Room.updateOne({"_id": responses[0]._id},
                     {
@@ -269,7 +269,7 @@ room.createRoom = function(condObj){
                 }).catch(err => {
                     reject(err);
                 });
-              }
+              //}
 
                 // }
               }
